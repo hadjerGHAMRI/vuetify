67c4f6ce1c6580f48125ce1c13241120d97e7d2c
yanisG <chetouaniyanis@gmail.com>
2022-11-19 03:37:44 +0100

    added logo neaper and removed email

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,26 @@
 
 <template>
   <div class="header">
-    <v-container class="black mx-auto" fluid style="height: 100px">
+    <v-container class="black mx-auto" fluid style="height: 120px">
       <v-layout row wrap justify-center>
-        <v-flex xs3 sm3 md2>
-          <H1 class="white--text">NEAPER</H1>
+        <v-flex xs3 sm3 md2 ma-5>
+          <img src="/neaper_logo.png" height="20" class="white--text darken-4" />
         </v-flex>
-        <v-flex xs6 sm6 offset-sm3  mt-16>
-        <v-avatar size="70" class="grey lighten-2">
-          <img :src="'data:image/jpeg;base64,'+profilePic">
-        </v-avatar>
-      </v-flex>
+        <v-flex xs6 sm6 offset-sm3 mt-16>
+          <v-avatar size="70" class="grey lighten-2">
+            <img :src="'data:image/jpeg;base64,' + profilePic" />
+          </v-avatar>
+        </v-flex>
       </v-layout>
     </v-container>
-    
+
     <v-container class="mx-auto mt-10">
       <v-layout row wrap justify-center>
-        <v-flex  xs7 sm4 md3 lg2 xl1>
-          <H2>{{name}}</H2>
+        <v-flex xs7 sm4 md3 lg2 xl1>
+          <H2>{{ name }}</H2>
         </v-flex>
       </v-layout>
     </v-container>
-    <v-container class="mx-auto" >
-      <v-layout row wrap justify-center>
-        <v-flex xs6 sm3 md3 lg2 xl1>
-          <v-subheader >{{email}}chetouani@gmail.com</v-subheader>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    
   </div>
 </template>
 <script>
@@ -47,8 +39,8 @@ export default {
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-     this.name=docSnap.data().user_name;
-     this.profilePic=docSnap.data().profile_pic;
+      this.name = docSnap.data().user_name;
+      this.profilePic = docSnap.data().profile_pic;
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
